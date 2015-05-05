dbb6081e6c8f32b6fa93372e6e8d6edad3e7b88d
jjregist <jjregist@gmail.com>
2015-05-05 12:43:49 -0400

    Add ID

--- app/models/reading.js
+++ app/models/reading.js
@@ -2,6 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ReadingSchema   = new Schema({
+	id: String,
 	value: Number, 
 	modified: { type: Date, default: Date.now }
 });
