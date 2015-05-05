89a65f8931093db6e926c1d43e616f0e3a329fbd
jjregist <jjregist@gmail.com>
2015-05-05 12:39:10 -0400

    cleanup the saved data

--- app/models/reading.js
+++ app/models/reading.js
@@ -3,7 +3,7 @@ var Schema       = mongoose.Schema;
 
 var ReadingSchema   = new Schema({
 	value: Number, 
-	modified: { type: Date, default: Date.now }
+	modified: Date.now 
 });
 
 module.exports = mongoose.model('Reading', ReadingSchema);
