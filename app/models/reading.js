7bfef661bdce0434ef182af71b204ad53e42f967
jjregist <jjregist@gmail.com>
2015-05-05 12:29:27 -0400

    back to basics

--- app/models/reading.js
+++ app/models/reading.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ReadingSchema   = new Schema({
-	name: String
+	value: String
 });
 
 module.exports = mongoose.model('Reading', ReadingSchema);
